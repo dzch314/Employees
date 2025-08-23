a27bee1cb8dcb96232fbb9ddf9de9a67f8088115
Dmitry Chernyavsky <dzch314@gmail.com>
2025-08-23 09:29:18 +0300

    Fix Eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -43,6 +43,7 @@ export default defineConfig([
 
     languageOptions: {
       globals: {
+        ...globals.node,
         ...globals.browser,
         ...globals.jest,
         __IS_DEV__: true,
@@ -75,6 +76,7 @@ export default defineConfig([
       '@typescript-eslint/ban-ts-comment': 'off',
       '@typescript-eslint/no-empty-object-type': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-require-imports': 'warn',
       '@typescript-eslint/no-unused-vars': 'off',
       'func-names': 'warn',
       'import/default': 'off',
